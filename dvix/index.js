d13aa80d6779098687d3174006e48d7cbd807bbf
litao.lt <litao.lt@alibaba-inc.com>
2014-01-20 13:03:34 +0800

    edit name.html

---
+++ dvix/index.js
@@ -0,0 +1,43 @@
+window.Site = {
+    local : !! ~location.search.indexOf('local'),
+    daily : !! ~location.search.indexOf('daily'),
+    debug : !! ~location.search.indexOf('debug'),
+    build : !! ~location.search.indexOf('build')
+};
+
+if(  (/daily.taobao.net/g).test(location.host)  ){
+    Site.daily = true;
+}
+
+var canvaxVersion = "1.0.1";
+var canvaxUrl     = "http://g.tbcdn.cn/thx/canvax/"+ canvaxVersion +"/";
+if( Site.daily ){
+    canvaxUrl     = "http://g.assets.daily.taobao.net/thx/canvax/" + canvaxVersion + "/";
+}
+
+KISSY.config({
+    packages: [{
+        name  : 'canvax' , 
+        path  :  canvaxUrl,
+        debug :  Site.debug
+    }]
+});
+
+function canvasSupport() {
+    return !!document.createElement('canvas').getContext;
+};
+KISSY.add("dvix/index" , function( S , Canvax ){
+
+    var Dvix = {
+        Canvax : Canvax
+    };
+
+    return Dvix;
+
+} , {
+    requires : [
+      "canvax/"
+    ]
+   
+})
+
