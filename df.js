d5044313c3c57f56979b879157fd934e1dc2b66b
litao.lt <litao.lt@alibaba-inc.com>
2015-01-08 15:08:03 +0800

    mod reset method

---
+++ df.js
@@ -0,0 +1,38 @@
+[
+   [ "渠道信息" , {title:"数量指标",subtitles:["展现uv","展现次数","覆盖人群"]}],
+   {data : [ "钻石展位" , [1,2,3] ],children:[]},
+   {
+     data : [ "钻石展位" , [1,2,3] ],
+     children : [
+        [ "钻石展位" , [1,2,3] ],
+        [ "钻石展位" , [1,2,3] ]
+     ]
+   }
+   
+]
+
+
+
+
+name    : 渠道信息
+showUv  : 展现uv
+showNum : 展现次数
+populaCover : 目标人群覆盖率
+clickRate : 点击率
+exposureNum : 人均曝光次数
+searchRate : 回搜率
+
+[
+  {name:"钻石展位",showUv:100,showNum:100,populaCover:100,clickRate:20,exposureNum:10,searchRate:10},
+  {
+      name:"钻石展位",showUv:100,showNum:100,populaCover:100,clickRate:20,exposureNum:10,searchRate:10
+      children : [
+          {name:"钻石展位",showUv:100,showNum:100,populaCover:100,clickRate:20,exposureNum:10,searchRate:10}
+      ]
+  },
+]
+
+1，阿里妈妈o2o-cms系统开发上线。
+2，canvax 图形渲染引擎的完善。
+3，chartx 基于canvax引擎的图表库开发丰富完善和在业务中应用
+4，基于canvax在业务上面一些轻度动画效果开发（比如应用在dmp标签市场上面的波浪效果）
