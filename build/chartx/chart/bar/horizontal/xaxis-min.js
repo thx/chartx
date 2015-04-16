39788b58d48d95ef76674f28b60537180201ba31
shenhua19850119 <fengchun@taobao.com>
2015-04-16 11:29:27 +0800

    1.6.7

--- build/chartx/chart/bar/horizontal/xaxis-min.js
+++ build/chartx/chart/bar/horizontal/xaxis-min.js
@@ -1,1 +1,1 @@
-define("chartx/chart/bar/horizontal/xaxis",["chartx/components/xaxis/xAxis_h"],function(a){var b=function(){this.xDis1=0,b.superclass.constructor.apply(this,arguments)};return Chartx.extend(b,a,{}),b});
+define("chartx/chart/bar/horizontal/xaxis",["chartx/components/xaxis/xAxis_h"],function(a){var b=function(a,c){this.xDis1=0,b.superclass.constructor.apply(this,arguments)};return Chartx.extend(b,a,{}),b});
