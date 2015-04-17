3565546dfa063da5f3230c47e73ad0f398eeb221
litao.lt <litao.lt@alibaba-inc.com>
2015-04-17 16:29:55 +0800

    mod line

---
+++ _site/chartx/Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+  //配置参数
+  grunt.initConfig({
+     clean : {
+         build : {
+             src : "./build" 
+         }
+     },
+     uglify: {
+         options: {
+            beautify: {
+                 ascii_only: true
+            }
+         },
+         app: {
+             expand: true,
+             cwd: "build/chartx/",
+             src: ['**/*.js', '!**/*-min.js'],
+             dest: "build/chartx/",
+             ext: '-min.js' // '-min.js'
+         }
+     },
+     copy: {
+         main: {
+             files: [
+                { expand: true, cwd: 'chartx', src: ['**','!index.js'], dest: 'build/chartx'},
+                { expand: true, cwd: 'demo', src: ['**'], dest: 'build/demo'}
+             ]
+         }
+      },
+      concat : {
+
+         options: {
+             separator: '\n\n',
+             process: function(src, filepath) {
+                 var develop = /\/\/BEGIN\(develop\)[\s\S]+?\/\/END\(develop\)/mg;
+                 return src.replace( develop , "" );
+             }
+         },
+         dist : {
+             src: [
+                 'chartx/index.js'
+             ],
+             dest: 'build/chartx/index.js'
+         }
+      }
+  });
+ 
+  //载入concat和uglify插件，分别对于合并和压缩
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+ 
+  //注册任务
+  grunt.registerTask('build', [ 'clean' , 'copy' , 'concat' , 'uglify'  ]);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  })
+
+  grunt.loadTasks('_tasks')
+}
---
+++ _site/chartx/demo/pie/index.js
@@ -0,0 +1,103 @@
+﻿KISSY.ready(function () {
+    KISSY.use("node" , function(){
+    
+    var S = KISSY;
+    var data1 = [
+        ['Firefox', 20],
+        ['Opera2', 1],        
+        ['Opera2', 1],
+        ['Opera3', 1],
+        ['Opera4', 1],
+        ['Opera5', 1],
+        ['Opera2', 1],
+        ['Opera2', 1],
+        ['Opera3', 1],
+        ['Opera4', 1],
+        ['Opera5', 1],
+        ['Firefox', 20],
+        ['Firefox', 20],
+        ['Opera2', 1],
+        ['Opera2', 1],
+        ['Opera3', 1],
+        ['Opera4', 1],
+        ['Opera5', 1],
+        ['Opera2', 1],
+        ['Opera2', 1],
+        ['Opera3', 1],
+        ['Opera4', 1],
+        ['Opera5', 1],
+        ['Firefox', 20]
+    ];
+    var options = {
+        //浮动tip
+        /*
+        tip: {
+            enabled: true,
+            format: '{point.title}\n{point.percentage}'
+        },
+        */
+        tips : {
+            //enabled  : false,
+            content : function(info){
+                return "xxx"
+
+            }
+        },
+        //周边tip
+        dataLabel: {
+            enabled: true,
+            format: '{point.name} : {point.percentage}'
+        },
+        //是否允许扇形选取
+        allowPointSelect: true,
+        cursor: 'pointer',
+        //是否允许动画
+        animation: true,
+        //内圆半径
+        innerRadius: 40,
+        strokeWidth: 2
+        //颜色序列,若不设置，会有默认的颜色序列
+        //colors:['red', 'yellow', 'blue']
+    }
+
+    Chartx.create.pie("canvasTest" , data1 , options).then(function( pie ){
+        pie.on('focused', function (e) {
+
+        })
+        pie.on('complete', function () {
+            //使用getList方法获取圆基础信息
+            /*
+            {
+            name:名称
+            index:索引
+            r:半径
+            color:颜色
+            percentage:百分比
+            }
+            */
+            var list = pie.getList();
+            var ul, li;
+            var legendContainer = S.all("#pieLegend");
+            legendContainer.empty();
+
+            ul = S.all('<ul></ul>');
+            legendContainer.append(ul);
+
+            for (var i = 0; i < list.length; i++) {
+                var item = list[i];
+                li = S.all('<li id="list_' + item.index + '" index="' + item.index + '" style="color:' + item.color + '"><span style="background-color:' + item.color + '"></span>' + item.name + '</li>');
+                ul.append(li);
+            }
+
+            //使用show方法控制扇形的显示与隐藏
+            legendContainer.all('li').on('click', function (e) {
+                var index = e.target.getAttribute('index');
+                pie.show(index);
+            });
+        });
+
+        pie.draw();
+    })
+ })
+
+});
