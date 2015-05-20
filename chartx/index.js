21e5e0554acdd0eefdf001a82e5d917eda47aaee
litao.lt <litao.lt@alibaba-inc.com>
2015-05-20 12:56:57 +0800

    add tree demo

--- chartx/index.js
+++ chartx/index.js
@@ -4,12 +4,8 @@ var Chartx = {
     start   : function () {
         //业务代码部分。
         //如果charts有被down下来使用。请修改下面的 
-<<<<<<< HEAD
 
-        var canvaxVersion = "2015.05.19";
-=======
         var canvaxVersion = "2015.05.20";
->>>>>>> daily/1.7.3
 
         //BEGIN(develop)
         if ((/daily.taobao.net/g).test(location.host)) {
