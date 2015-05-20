7f1183f2bdfd46a786a657754cc9a59cd897255a
litao.lt <litao.lt@alibaba-inc.com>
2015-05-20 16:09:48 +0800

    add tree-h demo

--- chartx/chart/topo/tree.js
+++ chartx/chart/topo/tree.js
@@ -305,8 +305,13 @@ define(
                 if( this.graph.rankdir == "TB" ){
                     //如果是从上到下的结构,默认， 目前也先只做这个模式
                     begin = [ dataNode.width/2 , dataNode.height ];
-                    end   = [ dataNode.width/2 , dataNode.height + this.graph.ranksep * 5 / 10  ]
+                    end   = [ dataNode.width/2 , dataNode.height + this.graph.ranksep / 2  ];
                 };
+                if( this.graph.rankdir == "LR" ){
+                    //如果是从左到右模式
+                    begin = [ dataNode.width , dataNode.height / 2 ];
+                    end   = [ dataNode.width + this.graph.ranksep / 2 , dataNode.height / 2  ];
+                }
 
                 var link = new Line({
                     id   : "tail_"+nodeId,
@@ -429,13 +434,23 @@ define(
             _getLinkPath : function( wbegin , wvControl , vTailPoint ){
                 var me = this;
                 var path = "M"+wbegin.x+"," + wbegin.y;
-                if( vTailPoint.x == wbegin.x ){
+                if( vTailPoint.x == wbegin.x && (me.graph.rankdir == "TB" || me.graph.rankdir == "BT" ) || 
+                    vTailPoint.y == wbegin.y && (me.graph.rankdir == "LR" || me.graph.rankdir == "RL") 
+                ){
                     path+= "L"+vTailPoint.x+","+vTailPoint.y;   
                 } else {
-                    path+= "L"+wbegin.x+"," + (wvControl.y + me.link.r);
-                    path+= "Q"+wbegin.x+"," + wvControl.y + ",";
-                    path+= (( vTailPoint.x > wbegin.x ? 1 : -1 )*me.link.r + wbegin.x ) + "," + wvControl.y;
-                    path+= "L"+vTailPoint.x+","+vTailPoint.y
+                    if( me.graph.rankdir == "TB" ){
+                        path += "L"+wbegin.x+"," + (wvControl.y + me.link.r);
+                        path += "Q"+wbegin.x+"," + wvControl.y + ",";
+                        path += (( vTailPoint.x > wbegin.x ? 1 : -1 ) * me.link.r + wbegin.x ) + "," + wvControl.y;
+                        path += "L"+vTailPoint.x+","+vTailPoint.y
+                    } 
+                    if( me.graph.rankdir == "LR" ){
+                        path += "L" + ( wvControl.x + me.link.r ) + "," + wbegin.y;
+                        path += "Q" + wvControl.x + "," + wvControl.y + ",";
+                        path += wvControl.x + "," + ( (vTailPoint.y > wbegin.y ? 1 : -1) * me.link.r + wvControl.y );
+                        path += "L"+vTailPoint.x+","+vTailPoint.y;
+                    }
                 }
                 return path;
             },
@@ -470,7 +485,6 @@ define(
                     }))
                     */
 
-
                 });
             
                 me.g.edges().forEach(function(e) {
@@ -478,15 +492,25 @@ define(
                     var edge  = me.g.edge(e);
                     var vnode = me.g.node(e.v); //这个link的父端节点
 
-                    var vTailPoint = {x:vnode.x , y:vnode.y+vnode.height/2+me.graph.ranksep*5/10};//父节点的尾巴末端
                     var wbegin     = edge.points[2];//子节点的发射点
-                    var wvControl  = {x:wbegin.x , y:vTailPoint.y}; //从wbegin 发射到vTailPoint 的控制折点
+
+                    var vTailPoint = {x:0 , y:0};//父节点的尾巴末端
+                    var wvControl  = {x:0 , y:0}; //从wbegin 发射到vTailPoint 的控制折点
+                    if( me.graph.rankdir == "TB" ){
+                        vTailPoint = { x : vnode.x , y : vnode.y + vnode.height/2 + me.graph.ranksep / 2 };
+                        wvControl  = { x : wbegin.x , y:vTailPoint.y}; 
+                    }
+                    if( me.graph.rankdir == "LR" ){
+                        vTailPoint = { x : vnode.x + vnode.width / 2 + me.graph.ranksep / 2 , y : vnode.y  };
+                        wvControl  = { x : vTailPoint.x , y:wbegin.y};
+                    }
                     
                     var lc = me.linksSp.getChildById("link_"+e.v+"_"+e.w).context;
                     lc.path = me._getLinkPath( wbegin , wvControl , vTailPoint );
 
+                    
                     /*
-                    KISSY.each( edge.points , function(point,i){
+                    _.each( edge.points , function(point,i){
                         var colors=["red","blue","green"];
                     
                        me.sprite.addChild(new Circle({
@@ -499,6 +523,7 @@ define(
                        }))
                     } );
                     */
+                    
                     
                 });
             },
