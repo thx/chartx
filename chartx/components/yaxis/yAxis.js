0cb53292e12223bad5ba4e14da0015c6ab1a63e1
litao.lt <litao.lt@alibaba-inc.com>
2015-06-09 18:53:05 +0800

    给line折线添加yAxis双轴线功能

--- chartx/components/yaxis/yAxis.js
+++ chartx/components/yaxis/yAxis.js
@@ -7,7 +7,8 @@ define(
         'chartx/utils/datasection'
     ],
     function( Canvax , Line , Tools , DataSection){
-        var yAxis = function(opt , data){
+        var yAxis = function(opt , data ){
+            
             this.w = 0;
             this.enabled = 1;//true false 1,0都可以
             this.dis  = 6                                  //线到文本的距离
@@ -16,13 +17,17 @@ define(
                     width   : 6,
                     lineWidth  : 3,
                     strokeStyle   : '#BEBEBE'
-            }
+            };
             this.text = {
                     fillStyle : '#999999',
                     fontSize  : 12,
-                    textAlign : "right",
                     format    : null
-            }
+            };
+            this.pos         = {
+                x : 0 , y : 0
+            };  
+            this.place       = "left";                       //yAxis轴默认是再左边，但是再双轴的情况下，可能会right
+            this.biaxial     = false;                        //是否是双轴中的一份
             this.layoutData  = [];                           //dataSection对应的layout数据{y:-100, content:'1000'}
             this.dataSection = [];                           //从原数据dataOrg 中 结果datasection重新计算后的数据
             this.dataOrg     = [];                           //源数据
@@ -101,14 +106,20 @@ define(
                 return dis
             },
             _initData  : function( data ){ 
-                
-                var arr = _.flatten( data.org ); //Tools.getChildsArr( data.org );
-                this.dataOrg     = data.org;
+                var arr = [];
+                if( !this.biaxial ){
+                    arr = _.flatten( data.org ); //Tools.getChildsArr( data.org );
+                } else {
+                    if( this.place == "left" ){
+                        arr = data.org[0];
+                    } else {
+                        arr = data.org[1];
+                    }
+                }
+
+                this.dataOrg     = arr;//data.org;
                
                 if( this.dataSection.length == 0 ){
-                    //if( !this.enabled ){
-                    //    arr.unshift( 0 );
-                    //} 
                     this.dataSection = DataSection.section( arr , 3 );
                 };
 
@@ -157,11 +168,11 @@ define(
                     var txt = new Canvax.Display.Text( content ,
                        {
                         context : {
-                            x  : x,
+                            x  : x + ( self.place == "left" ? 0 : 5 ),
                             y  : y + ( a == 0 ? -3 : 0 ),
                             fillStyle    : self.text.fillStyle,
                             fontSize     : self.text.fontSize,
-                            textAlign    : self.text.textAlign,
+                            textAlign    : self.place == "left" ? "right" : "left",
                             textBaseline : "middle"
                        }
                     });
@@ -173,7 +184,7 @@ define(
                         //线条
                         var line = new Line({
                             context : {
-                                x           : 0 + self.dis,
+                                x           : 0 + ( self.place == "left" ? +1 : -1 ) * self.dis,
                                 y           : y,
                                 xEnd        : self.line.width,
                                 yEnd        : 0,
