21c98b7383f3fae584361e34e494ef3bb1722983
litao.lt <litao.lt@alibaba-inc.com>
2015-06-11 18:20:23 +0800

    添加bar的叠加

--- build/chartx/utils/dataformat.js
+++ build/chartx/utils/dataformat.js
@@ -1,16 +1,16 @@
+/**
+* 把原始的数据
+* field1 field2 field3
+*   1      2      3
+*   2      3      4
+* 这样的数据格式转换为内部的
+* [{field:'field1',index:0,data:[1,2]} ......]
+* 这样的结构化数据格式。
+*/
 define(
     "chartx/utils/dataformat",
     [],
     function(){
-        /**
-         * 把原始的数据
-         * field1 field2 field3
-         *   1      2      3
-         *   2      3      4
-         * 这样的数据格式转换为内部的
-         * [{field:'field1',index:0,data:[1,2]} ......]
-         * 这样的结构化数据格式。
-         */
         return function( data , opt ){
     
               var dataFrame  = {    //数据框架集合
@@ -51,31 +51,24 @@ define(
                     total.push(o);
                 }
  
-                /*
-                for(var a = 1, al = arr.length; a < al; a++){
-                    for(var b = 0, bl = arr[a].length; b < bl; b++){
-                        total[b].data.push(arr[a][b]);
-                    }     
-                }
-                */
     
                 dataFrame.data = total;
                 //已经处理成[o,o,o]   o={field:'val1',index:0,data:[1,2,3]}
     
                 var getDataOrg = function( $field , totalList, type ){
-                    var arr = _.filter( totalList , function( obj ){
-                        return _.some( $field , function( field ){
-                            return obj.field == field;
-                        } ); 
-                    } );
+                    var arr = totalList;
                     //这个时候的arr只是totalList的过滤，还没有完全的按照$field 中的排序
                     var newData = [];
                     for( var i=0,l=$field.length; i<l ; i++ ){
-                        for( var ii=0,iil=arr.length ; ii<iil ; ii++ ){
-                             if( $field[i] == arr[ii].field ){
-                                 newData.push( arr[ii].data );
-                                 break;
-                             }
+                        if( _.isArray($field[i]) ){
+                            newData.push( getDataOrg( $field[i] , totalList , type ) );
+                        } else {
+                            for( var ii=0,iil=arr.length ; ii<iil ; ii++ ){
+                                 if( $field[i] == arr[ii].field ){
+                                     newData.push( arr[ii].data );
+                                     break;
+                                 }
+                            }
                         }
                     }
                     return newData;
@@ -114,11 +107,11 @@ define(
                 }             
                 dataFrame.yAxis.field = yField;
 
-
+                var allYFields = _.flatten(dataFrame.yAxis.field);
                 for(var a = 1, al = arr.length; a < al; a++){
                     for(var b = 0, bl = arr[a].length; b < bl; b++){
                         var val = arr[a][b];
-                        if( _.indexOf( dataFrame.yAxis.field , arr[0][b] ) >= 0 ){
+                        if( _.indexOf( allYFields , arr[0][b] ) >= 0 ){
                             //yAxis的数据需要Number
                             val = Number( val );
                         }
@@ -126,12 +119,10 @@ define(
                     }     
                 }
 
-
                 dataFrame.xAxis.org = getDataOrg( xField , total );
-                dataFrame.yAxis.org = getDataOrg( yField , total );
+                dataFrame.yAxis.org = getDataOrg( yField , total , "yAxis");
 
                 return dataFrame;
             }
-
     }
-)
+);
