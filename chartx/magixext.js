1273f6d965753f94d7abce00a8e03753d56d1962
litao.lt <litao.lt@alibaba-inc.com>
2015-06-16 15:41:25 +0800

    187

--- chartx/magixext.js
+++ chartx/magixext.js
@@ -6,55 +6,54 @@ define(
     ],
     function( View ){
         View=View.View||View;
-        View.mixin({
-            createChart : function( opt ){
+        View.prototype.createChart = function( opt ){
 
-                var type , el , data , opts ;
+            var type , el , data , opts ;
 
-                var args = arguments;
-                if( args.length > 1 && !_.isObject(args[0]) ){
-                    type = args[0];
-                    el   = args[1];
-                    data = args[2];
-                    opts = args[3];
-                } else {
-                    type = opt.type;
-                    el   = opt.el;
-                    data = opt.data; 
-                    opts = opt.opts;
-                };
+            var args = arguments;
+            if( args.length > 1 && !_.isObject(args[0]) ){
+                type = args[0];
+                el   = args[1];
+                data = args[2];
+                opts = args[3];
+            } else {
+                type = opt.type;
+                el   = opt.el;
+                data = opt.data; 
+                opts = opt.opts;
+            };
 
-                var me  = this;
-                var obj =  {
-                    then : function(fn){
-                        if( this.chart ){
-                            _.isFunction( fn ) && fn( this.chart );
-                            return this; 
-                        };
+            var me  = this;
+            var obj =  {
+                then : function(fn){
+                    if( this.chart ){
+                        _.isFunction( fn ) && fn( this.chart );
+                        return this; 
+                    };
 
-                        this._promiseHand.push( fn );
-                        return this;
-                    },
-                    chart : null,
-                    _promiseHand : []
-                };
-                var query = window.KISSY ? KISSY.all : $;
-                el = el.replace(/(^\s*)|(\s*$)/g, "");
-                if( el.slice(0,1)!= "#" && el.slice(0,1)!="." ){
-                    el = "#"+el;
-                };
-                query = query("#"+this.id+" " + el);
-                me.manage( Chartx.create[ type ]( query , data , opts ).then(function( chart ){
-                    obj.chart = chart;
-                    _.each( obj._promiseHand , function( fn ){
-                        _.isFunction( fn ) && fn( chart );
-                    } );
-                    obj._promiseHand = [];
-                }) );
+                    this._promiseHand.push( fn );
+                    return this;
+                },
+                chart : null,
+                _promiseHand : []
+            };
+            var query = window.KISSY ? KISSY.all : $;
+            el = el.replace(/(^\s*)|(\s*$)/g, "");
+            if( el.slice(0,1)!= "#" && el.slice(0,1)!="." ){
+                el = "#"+el;
+            };
+            query = query("#"+this.id+" " + el);
+            me.manage( Chartx.create[ type ]( query , data , opts ).then(function( chart ){
+                obj.chart = chart;
+                _.each( obj._promiseHand , function( fn ){
+                    _.isFunction( fn ) && fn( chart );
+                } );
+                obj._promiseHand = [];
+            }) );
 
-                return obj;
+            return obj;
 
-            }
-        });
+        }
+        
     }
 );
