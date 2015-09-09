ef9d3049c50d789ad354df8d93a421cf7bfe0b51
litao.lt <litao.lt@alibaba-inc.com>
2015-09-09 12:18:31 +0800

    bug fix

--- chartx/chart/bar/horizontal.js
+++ chartx/chart/bar/horizontal.js
@@ -27,7 +27,7 @@ define(
                 this._initModule();                        //初始化模块
                 this._startDraw({ w : this.height , h : this.width });                         //开始绘图
                 this._horizontal();
-                this._drawEnd();                           //绘制结束，添加到舞台
+                this._drawEnd();                         //绘制结束，添加到舞台
             },
             _horizontal : function(){
                 var me = this;
