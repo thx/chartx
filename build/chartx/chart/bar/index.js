81e4530a1803e8b375b9db27c163d290dc6326f8
litao.lt <litao.lt@alibaba-inc.com>
2015-12-21 19:15:35 +0800

    添加行业资产顶部图片

--- build/chartx/chart/bar/index.js
+++ build/chartx/chart/bar/index.js
@@ -1139,6 +1139,9 @@ define(
                         if (parseInt(range.start) == parseInt(me.dataZoom.range.start) && parseInt(range.end) == parseInt(me.dataZoom.range.end)) {
                             return;
                         };
+                        if( me.dataZoom.end <= me.dataZoom.start ){
+                            me.dataZoom.end = me.dataZoom.start+1;
+                        };
 
                         me.dataZoom.range.start = parseInt(range.start);
                         me.dataZoom.range.end = parseInt(range.end);
