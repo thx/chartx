b5f5e76736ce4fbd13a9d762a26890faf16dfb8b
litao.lt <litao.lt@alibaba-inc.com>
2016-01-21 21:45:35 +0800

    de

--- build/chartx/index-kimi.js
+++ build/chartx/index-kimi.js
@@ -2083,7 +2083,7 @@ define(
 
                 if (this.text.rotation != 0 && this.text.rotation % 90 == 0) {
                     this.isH = true;
-                }
+                };
 
                 this._initData(data , data1);
                 this.sprite = new Canvax.Display.Sprite();
@@ -2209,7 +2209,10 @@ define(
                         arr = _.flatten(d[1]);
                         this.field = _.flatten([this.field[1]]);
                     }
-                }
+                };
+                for( var i = 0, il=arr.length; i<il ; i++ ){
+                    arr[i] = Number( arr[i] );
+                };
                 return arr;
             },
             //data1 == [1,2,3,4]
--- build/chartx/index.js
+++ build/chartx/index.js
@@ -2283,7 +2283,7 @@ define(
 
                 if (this.text.rotation != 0 && this.text.rotation % 90 == 0) {
                     this.isH = true;
-                }
+                };
 
                 this._initData(data , data1);
                 this.sprite = new Canvax.Display.Sprite();
@@ -2409,7 +2409,10 @@ define(
                         arr = _.flatten(d[1]);
                         this.field = _.flatten([this.field[1]]);
                     }
-                }
+                };
+                for( var i = 0, il=arr.length; i<il ; i++ ){
+                    arr[i] = Number( arr[i] );
+                };
                 return arr;
             },
             //data1 == [1,2,3,4]
