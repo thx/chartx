e6b9271c8115b275431efda60f7141e0479edd82
litao.lt <litao.lt@alibaba-inc.com>
2016-12-09 16:27:18 +0800

    dev

--- build/chartx/lib/canvax/display/DisplayObject.js
+++ build/chartx/lib/canvax/display/DisplayObject.js
@@ -348,7 +348,6 @@ define(
     
                 var rotation = ctx.rotation;
                 if( rotation ){
-                    debugger
                     //如果有旋转
                     //旋转的原点坐标
                     var origin = new Point(ctx.rotateOrigin);
