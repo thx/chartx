a578c008af04a349ce8c59dfbcbebaaeae2c88f9
litao.lt <litao.lt@alibaba-inc.com>
2018-10-16 17:00:26 +0800

    dev

--- build/layout/utils/rebind.js
+++
@@ -1,1 +0,0 @@
-define("layout/utils/rebind",[],function(n,t,u){"use strict";function e(n,t,u){return function(){var e=u.apply(t,arguments);return e===t?n:e}}t.__esModule=!0,t.default=function(n,t){for(var u,r=1,i=arguments.length;++r<i;)n[u=arguments[r]]=e(n,t,t[u]);return n}});
--- build/theme.js
+++
@@ -1,1 +0,0 @@
-define("theme",["canvax"],function(e,c,t){"use strict";c.__esModule=!0;e("canvax").default._;c.default={colors:["#ff8533","#73ace6","#82d982","#e673ac","#cd6bed","#8282d9","#c0e650","#e6ac73","#6bcded","#73e6ac","#ed6bcd","#9966cc"],set:function(e){return this.colors=e,this.colors},get:function(){return this.colors}}});
