5bbd5c4e798ed5d5d2198e63475708e31865dd3a
litao.lt <litao.lt@alibaba-inc.com>
2018-12-14 13:45:34 +0800

    auto commit by alimama-deploy

--- rollup.config.js
+++ rollup.config.js
@@ -12,8 +12,8 @@ rollup.rollup({
         include : ['./src/**','node_modules/mmvis/**', "node_modules/canvax/**"]
       }),
       resolve({ jsnext: true, main: true, browser: true }), 
-      commonjs()
-      //uglify()
+      commonjs(),
+      uglify()
     ]
 }).then(function(bundle) {
 
